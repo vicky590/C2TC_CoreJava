4df5ab0d861f51472018494587c38d2f25520aad
ravir <ravir@Ravi>
2025-09-23 18:39:08 +0530

    day1 is completed!

---
+++ CorejavaPrograms/src/Day1/FirstProgram.java
@@ -0,0 +1,11 @@
+package Day1;
+
+public class FirstProgram {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		public static void main(String[] args) {
+			System.out.println("Hello World, Welcome to Java");
+	}
+
+}
--- CorejavaPrograms/src/Day1/hello.java
+++ CorejavaPrograms/src/Day1/hello.java
@@ -1,11 +1,12 @@
 package Day1;
-
-public class hello {
-
+public class hello{
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.print("Welcome Java...!");
+
+		for (int i = 5; i < 10; i++) {
+			if (i == 5)
+				break;
+			System.out.println(i);
+		}
 
 	}
-
 }
