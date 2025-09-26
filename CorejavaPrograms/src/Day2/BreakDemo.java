78648335ab1798e8d8dfbcdcd31b921773ad8187
ravir <ravir@Ravi>
2025-09-26 13:15:45 +0530

    Day2 completed!

---
+++ CorejavaPrograms/src/Day2/BreakDemo.java
@@ -0,0 +1,17 @@
+package Day2;
+
+public class BreakDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+ for(int i = 1; i< 5; i++) {
+	if(i == 4) {
+		break;}
+	else {
+		System.out.println(i);
+	}
+}
+
+}
+
+}
